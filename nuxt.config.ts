2f12f28be233efaca1a8f795f8bf5d269a244307
Mouli Bheemaneti <mouli.b@narayanagroup.com>
2024-02-04 10:56:09 +0530

    Added app details in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+
+  app: {
+    head: {
+      htmlAttrs: { lang: "en" },
+      title: "Mouli Bheemaneti - Personal Website",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      meta: [
+        { name: "description", content: "Mouli Bheemaneti is on a journey for versatility." },
+      ],
+    },
+  },
 })
