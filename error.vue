37d868c339e46223a5e4b232f223e23926d13000
Mouli Bheemaneti <mouli.b@narayanagroup.com>
2024-02-07 07:48:51 +0530

    Lighthouse errors and warning fixed.

---
+++ error.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Error page</div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss">
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,35 @@ export default defineNuxtConfig({
       viewport: "width=device-width, initial-scale=1",
       meta: [
         { name: "description", content: "Mouli Bheemaneti is on a journey for versatility." },
+        { name: "theme-color", content: "#000000" },
+      ],
+      link: [
+        {
+          // https://developer.chrome.com/docs/lighthouse/pwa/installable-manifest/?utm_source=lighthouse&utm_medium=devtools
+          href: "manifest.json",
+          rel: "manifest",
+        },
       ],
     },
   },
+
+  modules: [
+    "@nuxtjs/robots",
+  ],
+
+  robots: {
+    UserAgent: "*",
+    Disallow: "/",
+    BlankLine: true,
+    Comment: "Comment here",
+  },
+
+  nitro: {
+    compressPublicAssets: true,
+  },
+
+  css: [
+    "~/assets/scss/app.scss",
+  ]
+
 })
