e3189402e085c4a4e83594a6e4ebde25f7725b83
moulib-narayana <mouli.b@narayanagroup.com>
2024-06-01 14:40:23 +0530

    Packages upgraded and ESLint Removed.

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -3,3 +3,5 @@ export const getLocalDynamicImageUrl = (rootPath: string, name: string) => {
   return new URL(`/assets/${rootPath}/${name}`, import.meta.url).href
 
 }
+
+export const delay = (ms: number): Promise<void> => new Promise((res) => setTimeout(res, ms));
